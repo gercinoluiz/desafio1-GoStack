5e626e4144e948a98427a3e6adc7f9f85bcee3de
Gercino Luiz <gercino.neto.luiz@gmail.com>
2020-10-27 10:24:29 -0300

    changes

--- src/app.js
+++ src/app.js
@@ -1,33 +1,84 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes } = request.body;
+
+  const newRepo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(newRepo);
+
+  response.status(201).json(newRepo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  repositoryIndex < 0
+    ? response.status(400).json({ error: "Bad Request" })
+    : null;
+
+  repositories[repositoryIndex] = {
+    ...repositories[repositoryIndex],
+    title: title,
+    url: url,
+    techs: techs,
+  };
+
+  response.status(200).json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  repoIndex < 0 ? response.status(400).json({ error: "Bad Request" }) : null;
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs, likes } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    (reposotory) => reposotory.id === id
+  );
+
+  repositoryIndex < 0
+    ? response.status(400).json({ error: "Bad Request" })
+    : null;
+
+  repositories[repositoryIndex].likes++;
+
+  response.status(200).json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
